db56979f9ed46f9fa7e84ea94c633309b8ba80e2
Julius Torralba <Julius.Torralba@GMail.com>
2024-04-10 18:15:52 -0600

    Implement PATH & EXTENSION parameters.

--- Prune.ps1
+++ Prune.ps1
@@ -1,1 +1,17 @@
-﻿Get-ChildItem -Path "C:\EmergencyReporting\Out\*.xml" -Recurse $Folder -Force -ea 0 | ? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-90)} | Remove-Item
+﻿param ($Path = $NULL, $Extension = $NULL)
+
+if ($Path -ne $NULL -and $Extension -ne $NULL)
+{
+    if (Test-Path $Path)
+    {
+        $Target = $Path + "\*." + $Extension
+
+        Write-Host "Pruning " $Target
+
+        Get-ChildItem -Path $Target -Recurse $Folder -Force -ea 0 | ? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-90)} | Remove-Item
+    }
+}
+
+# Usage:
+
+# PowerShell -ExecutionPolicy Bypass "C:\Prune.ps1" "C:\Path" "extension"
